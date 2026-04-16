df25a6682c33868c8a34f16ff4e9c02857fd3d81
Tyler Robinson <billybotmk@gmail.com>
2026-04-16 14:33:46 -0400

    fix server

--- 4chess/game.js
+++ 4chess/game.js
@@ -1,4 +1,4 @@
-socket = io("http://localhost:3000");
+const socket = io("https://cheesechess-production.up.railway.app");
 
 function resignButton() {
   if (game.colorToMove === myColor) {
--- backend/server.js
+++ backend/server.js
@@ -179,7 +179,7 @@ function sendInChat(txt) {
 }
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
